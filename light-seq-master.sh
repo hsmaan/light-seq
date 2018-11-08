6f6798edc7861f993c73cc81d768470e38c12e73
hyhy8181994 <hyhy8181994@gmail.com>
2018-11-08 10:13:22 -0500

    Update light-seq-master.sh
    add argument selection to bwa alignment

--- light-seq-master.sh
+++ light-seq-master.sh
@@ -16,18 +16,21 @@ read al_selection
 echo "single end or paired end"
 read sq_type
 
+echo "any arguments to add? please leave space between each arugments"
+read arg
+
 if [ "$al_selection" == "MEM" ];then
 echo "you choose bwa mem"
 if [ "$sq_type" == "single" ];then
 cd $DATA
-parallel -j $CPU $BWA mem -t $THR $REF {}.fastq ">" {}.sam ::: $(ls -1 *.fastq | sed 's/.fastq//')
+parallel -j $CPU $BWA mem -t $THR $arg $REF {}.fastq ">" {}.sam ::: $(ls -1 *.fastq | sed 's/.fastq//')
 if [ $? -ne 0 ]
 			then
 				printf There is a problem in the alignment step
 				exit 1
 		fi
 elif [ "$sq_type" == "pair" ];then
-parallel -j $CPU $BWA mem -t $THR $REF {}_1.fastq {}_2.fastq">" {}.sam ::: $(ls -1 *.fastq | sed 's/.fastq//')
+parallel -j $CPU $BWA mem -t $THR $arg $REF {}_1.fastq {}_2.fastq">" {}.sam ::: $(ls -1 *.fastq | sed 's/.fastq//')
 if [ $? -ne 0 ]
 			then
 				printf There is a problem in the alignment step
@@ -43,14 +46,14 @@ elif [ "$al_selection" == "aln" ];then
 echo "you choose bwa aln"
 if [ "$sq_type" == "single" ];then
 cd $DATA
-parallel -j $CPU $BWA aln -t $THR $REF {}.fastq ">" {}.sam ::: $(ls -1 *.fastq | sed 's/.fastq//')
+parallel -j $CPU $BWA aln -t $THR $arg $REF {}.fastq ">" {}.sam ::: $(ls -1 *.fastq | sed 's/.fastq//')
 if [ $? -ne 0 ]
 			then
 				printf There is a problem in the alignment step
 				exit 1
 		fi
 elif [ "$sq_type" == "pair" ];then
-parallel -j $CPU $BWA aln -t $THR $REF {}_1.fastq {}_2.fastq">" {}.sam ::: $(ls -1 *.fastq | sed 's/.fastq//')
+parallel -j $CPU $BWA aln -t $THR $arg $REF {}_1.fastq {}_2.fastq">" {}.sam ::: $(ls -1 *.fastq | sed 's/.fastq//')
 if [ $? -ne 0 ]
 			then
 				printf There is a problem in the alignment step
