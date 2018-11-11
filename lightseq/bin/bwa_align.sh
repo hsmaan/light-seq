208c3be01cc87a8547e3ccbdb30d0f7ca5708dc5
hyhy8181994 <hyhy8181994@gmail.com>
2018-11-11 14:03:23 -0500

    Add files via upload
    fixed the pair mode 
    file names of fastq file for pair mode have to be something_1.fastq somthing_2.fastq

--- lightseq/bin/bwa_align.sh
+++ lightseq/bin/bwa_align.sh
@@ -20,47 +20,49 @@ function bwa_align {
 	read arg
 
 	if [ "$al_selection" == "MEM" ];then
-		echo "You chose the BWA MEM alignment algorithm"
-		if [ "$sq_type" == "single" ];then
-			cd $DATA
-			parallel -j $CPU $BWA mem -t $THR $arg $REF {}.fastq ">" {}.sam ::: $(ls -1 *.fastq | sed 's/.fastq//')
-			if [ $? -ne 0 ]
-			then
-				printf There is a problem in the alignment step
-				exit 1
+	echo "You chose the BWA MEM alignment algorithm"
+	if [ "$sq_type" == "single" ];then
+	cd $DATA
+	parallel -j $CPU $BWA mem -t $THR $arg $REF {}.fastq ">" {}.sam ::: $(ls -1 *.fastq | sed 's/.fastq//')
+	if [ $? -ne 0 ]
+				then
+					printf There is a problem in the alignment step
+					exit 1
 			fi
-		elif [ "$sq_type" == "pair" ];then
-			parallel -j $CPU $BWA mem -t $THR $arg $REF {}_1.fastq {}_2.fastq">" {}.sam ::: $(ls -1 *_1.fastq | sed 's/_1.fastq//')
-			if [ $? -ne 0 ]
-			then
-				printf There is a problem in the alignment step
-				exit 1
+	elif [ "$sq_type" == "pair" ];then
+	cd $DATA
+	parallel -j $CPU $BWA mem -t $THR $arg $REF {}_1.fastq {}_2.fastq ">" {}.sam ::: $(ls -1 *_1.fastq | sed 's/_1.fastq//')
+	if [ $? -ne 0 ]
+				then
+					printf There is a problem in the alignment step
+					exit 1
 			fi
-		else
-			exit 1
-		fi
+	else
+	exit 1
+	fi
 
 
 	elif [ "$al_selection" == "aln" ];then
-		echo "You chose the BWA aln alignment algorithm"
-		if [ "$sq_type" == "single" ];then
-			cd $DATA
-			parallel -j $CPU $BWA aln -t $THR $arg $REF {}.fastq ">" {}.sam ::: $(ls -1 *.fastq | sed 's/.fastq//')
-			if [ $? -ne 0 ]
-			then
-				printf There is a problem in the alignment step
-				exit 1
+	echo "You chose the BWA aln alignment algorithm"
+	if [ "$sq_type" == "single" ];then
+	cd $DATA
+	parallel -j $CPU $BWA aln -t $THR $arg $REF {}.fastq ">" {}.sam ::: $(ls -1 *.fastq | sed 's/.fastq//')
+	if [ $? -ne 0 ]
+				then
+					printf There is a problem in the alignment step
+					exit 1
 			fi
-		elif [ "$sq_type" == "pair" ];then
-			parallel -j $CPU $BWA aln -t $THR $arg $REF {}_1.fastq {}_2.fastq">" {}.sam ::: $(ls -1 *_1.fastq | sed 's/_1.fastq//')
-			if [ $? -ne 0 ]
-			then
-				printf There is a problem in the alignment step
-				exit 1
+	elif [ "$sq_type" == "pair" ];then
+	cd $DATA
+	parallel -j $CPU $BWA aln -t $THR $arg $REF {}_1.fastq {}_2.fastq ">" {}.sam ::: $(ls -1 *_1.fastq | sed 's/_1.fastq//')
+	if [ $? -ne 0 ]
+				then
+					printf There is a problem in the alignment step
+					exit 1
 			fi
-		else
-			exit 1
-		fi	
+	else
+	exit 1
+	fi
 	else 
 	echo "Please select correct algorithm or mode"
 	fi 
