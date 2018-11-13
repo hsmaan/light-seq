f0113bc8a8433200a4e3db3114e4884fb4d30823
Hassaan Maan <40446083+hsmaan@users.noreply.github.com>
2018-11-13 17:48:58 -0500

    added log

--- lightseq/bin/demultiplex.sh
+++ lightseq/bin/demultiplex.sh
@@ -32,6 +32,9 @@ function demultiplex {
 	
 	## remove multiplexed file so it's not processed downstream
  	rm $MULTIPLXD_FILE
+	if [ $? -eq 0]; then
+		printf "The demultiplexing step using sabre completed successfully.\n" >> main.log
+	fi
 }
 
 demultiplex
