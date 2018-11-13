0d319f20e8de8335657882b334d98778c9afe550
hyhy8181994 <hyhy8181994@gmail.com>
2018-11-12 22:20:49 -0500

    Add files via upload
    finding file only with .fq end maybe problematic for other file type, so I let user to choose the file by simply entering the file name

--- lightseq/bin/demultiplex.sh
+++ lightseq/bin/demultiplex.sh
@@ -3,35 +3,37 @@
 
 
 function demultiplex {
-
-  cd #DATA
  
   #user input for tool and barcode
   echo -e "What is your sabre path?\n"
   local SABRE
   read SABRE
-
+  TOOL=$SABRE
 
   echo -e "\nWhat is the path to your barcode sequence?\n"
   local BAR
   read BAR
-  BARCODE=$BAR
+  BARCODE=$BAR  
   
-  
-  MULTIPLXD_FILE=$( ls $DATA/*.fq )
-  ##echo $MULTIPLXD_FILE
-  
-  if [ "$sq_type" == "single" ] ;
-  then
-    $SABRE se -f $DATA/*.fq -b $BARCODE -u unk.fq ;
-    	if [ $? -ne 0 ]; then
+  if [ "$seq_type" == "single" ];then
+  cd $DATA
+  ##Help user to local the file by simply enter the file name
+  echo -e "\nHere is the list of files in your chosen directory\n"
+  command ls $DATA
+  echo -e "\nPlease enter Name of your file for demultiplexing\n"
+  local NAME
+  read NAME
+  local FILE=`find $DATA -name $NAME`
+  $TOOL se -f $FILE -b $BARCODE -u *.fastq
+  if [ $? -ne 0 ]; then
 		printf "There is a problem in the sabre demultiplexing step"
 		exit 1
   	fi
+  
+  ####PLACEHOLDER FOR PAIRED END####
+  else
+  :
   fi
-
-  ## remove multiplexed file so it's not processed downstream
-  rm $MULTIPLXD_FILE
 }
 
 demultiplex
