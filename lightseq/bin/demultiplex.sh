7567a7b6f2569ec4dfad68bd8cf54f0eda9b813c
Aditya Alleear <38356489+aditalle@users.noreply.github.com>
2018-11-13 09:02:28 -0500

    put in code for pair end and nested stuff
    have not run code. but

--- lightseq/bin/demultiplex.sh
+++ lightseq/bin/demultiplex.sh
@@ -3,35 +3,34 @@
 
 
 function demultiplex {
-
-  cd #DATA
- 
-  #user input for tool and barcode
-  echo -e "What is your sabre path?\n"
-  local SABRE
-  read SABRE
-
-
-  echo -e "\nWhat is the path to your barcode sequence?\n"
-  local BAR
-  read BAR
-  BARCODE=$BAR
+	cd #DATA
+ 	#user input for tool and barcode
+	echo -e "What is your sabre path?\n"
+	local SABRE
+	read SABRE
+	
+	echo -e "\nWhat is the path to your barcode sequence?\n"
+	local BAR
+	read BAR
+	BARCODE=$BAR
+	
+	MULTIPLXD_FILE=$( ls $DATA/*.fq )
+	##echo $MULTIPLXD_FILE
   
-  
-  MULTIPLXD_FILE=$( ls $DATA/*.fq )
-  ##echo $MULTIPLXD_FILE
-  
-  if [ "$sq_type" == "single" ] ;
-  then
-    $SABRE se -f $DATA/*.fq -b $BARCODE -u unk.fq ;
-    	if [ $? -ne 0 ]; then
-		printf "There is a problem in the sabre demultiplexing step"
-		exit 1
+  	if [ "$sq_type" == "single" ]; then
+  		$SABRE se -f $DATA/*.fq -b $BARCODE -u unk.fq ;
+    		if [ $? -ne 0 ]; then
+			printf "There is a problem in the sabre demultiplexing step"
+			exit 1
+  		fi
+		
+	#this code should run under the assumption that the files are name _1 and _2 respectively, not R1 and R2. 
+  	elif ["$sq_type" = "pair"] ; then 
+  		$SABRE pe -f $DATA/*_1.fastq -r $DATA/*_2.fastq -b $BARCODE -u unk1.fq -w unk2.fq 
   	fi
-  fi
-
-  ## remove multiplexed file so it's not processed downstream
-  rm $MULTIPLXD_FILE
+	
+	## remove multiplexed file so it's not processed downstream
+ 	rm $MULTIPLXD_FILE
 }
 
 demultiplex
